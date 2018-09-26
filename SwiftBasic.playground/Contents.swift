55b7b48ee9167d44bad5b0de127ea269b112ec22
ninetyfivejae <jh33836782@gmail.com>
2018-09-26 13:50:30 +0900

    Add[SwiftBasic.playground] : 배열 및 딕셔너리 사용

--- SwiftBasic.playground/Contents.swift
+++ SwiftBasic.playground/Contents.swift
@@ -85,3 +85,38 @@ repeat{
     i += 1
     print(i)
 } while i < 10
+
+let month = 3
+let toDo = "Travel"
+
+var toDoArray: Array<String> = ["Travel", "Work", "Call"]
+var evenNumber: [Int] = [2,4,6,8]
+
+toDoArray[0] = "Play"
+toDoArray[1]
+toDoArray[2]
+
+evenNumber.append(10)
+evenNumber.insert(12, at: 0)
+evenNumber.remove(at: 0)
+print(evenNumber)
+
+for toDo in toDoArray{
+    print(toDo)
+}
+
+var toDoDic: Dictionary<Int, String>  = [17: "Play", 25: "Watch Movies", 28: "Travel"]
+var toDoDic2: Dictionary<String, String> = ["17": "Play", "25": "Watch Movies", "28": "Travel"]
+toDoDic2["17"]
+toDoDic2["31"] = "Study"
+print(toDoDic2)
+toDoDic2["31"] = ""
+print(toDoDic2)
+toDoDic2.removeValue(forKey: "31")
+print(toDoDic2)
+toDoDic2["28"] = nil
+toDoDic2.removeAll()
+print(toDoDic2)
+for (k, v) in toDoDic2{
+    print("key: \(k), value: \(v)")
+}
