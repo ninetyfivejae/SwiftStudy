1a2b36fe0b5b9e27611bf36358a4062c9363de1d
ninetyfivejae <jh33836782@gmail.com>
2019-01-25 01:48:43 +0900

    Add[SettingsApplication]: 연습으로 만들어보는 설정 애플리케이션 init

---
+++ SettingsApplication/SettingsApplication/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SettingsApplication
+//
+//  Created by 신재혁 on 25/01/2019.
+//  Copyright © 2019 ninetyfivejae. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
