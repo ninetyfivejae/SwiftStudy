4abcb12259a2b81fe25ccda7b4f8cb0121ef10e2
ninetyfivejae <jh33836782@gmail.com>
2019-02-13 17:46:24 +0900

    Add: 클로저 사용 예제 추가

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -76,3 +76,51 @@ print(++myPosition)
 
 //---------------------------------------------------
 
+//public func sorted(by areInIncreasingOrder: (Element, Element) -> Bool) -> [Element]
+
+let names: [String] = ["wizplan", "eric", "yagom", "jenny"]
+
+
+
+func backwards(first: String, second: String) -> Bool {
+    print("\(first) \(second) 비교 중")
+    return first > second
+}
+let firstReversed: [String] = names.sorted(by: backwards)
+print(firstReversed)
+
+
+
+let secondReversed: [String] = names.sorted(by: { (first: String, second: String) -> Bool in
+    return first > second
+})
+print(secondReversed)
+
+
+
+let thirdReversed: [String] = names.sorted() { (first: String, second: String) -> Bool in
+    return first > second
+}
+print(thirdReversed)
+
+
+
+let fourthReversed: [String] = names.sorted { (first: String, second: String) -> Bool in
+    return first > second
+}
+print(fourthReversed)
+
+
+
+let fifthReversed: [String] = names.sorted() {
+    return $0 > $1
+}
+print(fifthReversed)
+
+
+
+let sixthReversed: [String] = names.sorted { $0 > $1 }
+print(sixthReversed)
+
+//---------------------------------------------------
+
